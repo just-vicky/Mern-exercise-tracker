41639c612dd175705f2c61a915235205536bc608
Vikram <vikraman@auroville.org.in>
2023-11-23 17:25:47 +0530

    Create Exercise Log

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -35,7 +35,7 @@ router.route('/:id').delete((req, res) => {
     .then(exercise => res.json("Exercise deleted"))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-router.route('/update/:id').delete((req, res) => {
+router.route('/update/:id').post((req, res) => {
   Exercise.findById(req.params.id)
     .then(exercise => {
       exercise.username = req.body.username;
